df4160f3c82a1af55f64868e2421e6c2009dd646
mdsalehin <salahin1999@gmail.com>
2025-07-30 17:51:53 +0600

    Class and object

---
+++ src/CodeExamplesSolution/OOP/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+class Student
+{
+    //property 
+    public string name;
+    public int age;
+
+
+    //Funcionality
+    public void ShowInfo()
+    {
+        Console.WriteLine("Name: {0}, Age: {1}",name,age);
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        //Student class ar akta object create korechi.so object create krete hole akta Constructor_Create--->Student(); 
+        Student student = new Student();
+
+        //object Property Define
+        student.name = "salehin";
+        student.age = 25;
+        student.ShowInfo();
+
+    }
+}
